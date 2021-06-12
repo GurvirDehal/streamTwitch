c1d21a10e0d098cde31a1bb4d7a9fe58b7653aeb
soumya <d4l1b@cs.ubc.ca>
2021-06-12 15:11:25 -0700

    first commit

---
+++ app/src/main/java/com/streamtwitch/fetch.java
@@ -0,0 +1,11 @@
+package com.streamtwitch;
+
+public class fetch {
+
+    String clientId = "b31o4btkqth5bzbvr9ub2ovr79umhh";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
